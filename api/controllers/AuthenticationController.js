0bb1ec0d92b32ea14d442cf015a9afc395d96587
Bajso <Bajso@users.noreply.github.com>
2018-05-15 20:37:40 +0100

    Modified layout
    Layout is now done
    Changed redirect after login

--- api/controllers/AuthenticationController.js
+++ api/controllers/AuthenticationController.js
@@ -10,16 +10,14 @@ module.exports = {
           user
         });
       }
+
       req.logIn(user, function(err) {
         if (err) res.send(err);
-        return res.send({
-          message: info.message,
-          user
-        });
+        return res.redirect('/')
       });
     })(req, res);
   },
-  
+
   logout: function(req, res) {
     req.logout();
     res.redirect('login');
