34df95462112c80719b6fe5257e0abd0991830de
Bajso <Bajso@users.noreply.github.com>
2018-05-18 00:26:46 +0100

    Signup error classification

--- api/controllers/AuthenticationController.js
+++ api/controllers/AuthenticationController.js
@@ -26,7 +26,24 @@ module.exports = {
     // create user and store in a database
     User.create(req.allParams(), (err, user) => {
       if (err) {
-        return res.view('pages/signup', {errMessage: err});
+        let out = err.message;
+        if (out.includes('empty')) {
+          out = 'Please fill out all fields';
+        }
+        else if (out.includes('minimum')) {
+          out = 'Password must contain at least six characters';
+        }
+        else if (out.includes('valid email')) {
+          out = 'Please insert a valid email address';
+        }
+        else if (out.includes('uniqueness')) {
+          out = 'That username is already taken';
+        }
+        else {
+          out = 'Better luck next time';
+        }
+
+        return res.view('pages/signup', {errMessage: out});
       }
       return res.redirect('login');
     });
